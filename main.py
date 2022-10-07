596ffea6c5b8410f43bf45c5ed988b9884c97f10
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-07 23:26:47 +0600

    Shape of arrow changed to turtle by passing the shape into the class Turtle

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ screen = Screen()
 screen.setup(width=500, height=400) #width = 500pixel, height = 400pixel
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle do you think will win? Enter a color: ") #adds a popup window
 
-tim = Turtle()
+tim = Turtle(shape="turtle")
+
+tim.penup()
 tim.goto(x=-230, y=-100) #takes x coordinate and y coordinate amd moves turtle to that position
 
 
