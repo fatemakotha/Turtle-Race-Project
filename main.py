4f42bb19d938ec41e7fd35075bc5cab98b706188
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-07 23:17:46 +0600

    Added a pop up window usinf screen.textinput()

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
-screen.setup(500, 400) #width = 500pixel, height = 400pixel
+screen.setup(width=500, height=400) #width = 500pixel, height = 400pixel
+screen.textinput(title="Make your bet", prompt="Which turtle do you think will win? Enter a color: ") #adds a popup window
 
 
 
