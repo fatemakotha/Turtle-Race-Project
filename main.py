80a6ae549694a61f2f5be77aac46c076cf287dc1
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-07 23:37:14 +0600

    Created six turtles of 6 colors

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400) #width = 500pixel, height = 400pixel
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle do you think will win? Enter a color: ") #adds a popup window
+colors = ["red", "orange", "blue", "green", "yellow", "purple"]
+y_coordinate = [-70, -40, -10, 20, 50, 80]
 
-tim = Turtle(shape="turtle")
+for each_turtle in range(0, 6):
 
-tim.penup()
-tim.goto(x=-230, y=-100) #takes x coordinate and y coordinate amd moves turtle to that position
+    tim = Turtle(shape="turtle")
+    tim.color(colors[each_turtle])
+    tim.penup()
+    tim.goto(x=-230, y=-y_coordinate[each_turtle]) #takes x coordinate and y coordinate amd moves turtle to that position
 
+
+
+
+
+
+# jim = Turtle(shape="turtle")
+# jim.penup()
+# jim.goto(x=-230, y=-70) #takes x coordinate and y coordinate amd moves turtle to that position
 
 
 
