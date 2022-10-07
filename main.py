987d9aa5f18d1f3a15850c7972ad9849ebad3f55
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-08 00:20:58 +0600

    Completed

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 from turtle import Turtle, Screen
+import random
 
-
+is_race_on = False
 screen = Screen()
-screen.setup(width=500, height=400) #width = 500pixel, height = 400pixel
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle do you think will win? Enter a color: ") #adds a popup window
-colors = ["red", "orange", "blue", "green", "yellow", "purple"]
-y_coordinate = [-70, -40, -10, 20, 50, 80]
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-for each_turtle in range(0, 6):
+#Create 6 turtles
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
-    tim = Turtle(shape="turtle")
-    tim.color(colors[each_turtle])
-    tim.penup()
-    tim.goto(x=-230, y=-y_coordinate[each_turtle]) #takes x coordinate and y coordinate amd moves turtle to that position
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        # Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+        #230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor() #gets hold of the color of the winning turtle
+            if winning_color == user_bet: #matches the answer with the bet
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
 
 
 
-
-
-
-# jim = Turtle(shape="turtle")
-# jim.penup()
-# jim.goto(x=-230, y=-70) #takes x coordinate and y coordinate amd moves turtle to that position
+screen.exitonclick()
 
 
 
