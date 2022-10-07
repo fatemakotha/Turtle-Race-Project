8eb583316b8f55ac70ad89d554fe06a8fbe5bded
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-07 23:15:04 +0600

    Used screen.setup() to set width of screen to 500 pixel and height to 400pixel

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from turtle import Turtle, Screen
 
+tim = Turtle()
+screen = Screen()
+screen.setup(500, 400) #width = 500pixel, height = 400pixel
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
